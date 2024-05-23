82075533b865a0cc414b4be9337ae706a9567390
stasinosntaveas <ntaveasst@gmail.com>
2024-05-23 21:34:52 +0300

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,24 +1,22 @@
-DROP SCHEMA IF EXISTS `cooking_contest_ntua`;
+DROP SCHEMA if exists `cooking_contest_ntua`;
 CREATE SCHEMA `cooking_contest_ntua`;
-USE cooking_contest_ntua;
-
+use cooking_contest_ntua;
 
 
 DROP TABLE IF EXISTS food_group;
 CREATE TABLE food_group (
-	food_group_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+food_group_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     food_group_name VARCHAR(100) NOT NULL,
     food_group_description VARCHAR(500) NOT NULL,
     image varchar(100) not null check (image like 'https://%'),
     image_caption varchar(100) not null,
     PRIMARY KEY (food_group_id)
 );
-
 #CREATE UNIQUE INDEX food_group_pk ON food_group (food_group_id);
 
 DROP TABLE IF EXISTS cooking_ingredients;
 CREATE TABLE cooking_ingredients (
-	ingredients_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ingredients_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     ingredients_name VARCHAR(100) NOT NULL,
     measuring_herd VARCHAR(100) NOT NULL,
     food_group_id INT UNSIGNED NOT NULL,
@@ -35,11 +33,11 @@ CREATE TABLE cooking_ingredients (
 
 DROP TABLE IF EXISTS nutritions_info;
 CREATE TABLE nutritions_info (
-	ingredients_id INT UNSIGNED NOT NULL,
+ingredients_id INT UNSIGNED NOT NULL,
     carbonhydrates FLOAT NOT NULL CHECK (carbonhydrates >= 0),
-	lipids FLOAT NOT NULL CHECK (lipids >= 0),
-	proteins FLOAT NOT NULL CHECK (proteins >= 0),
-	PRIMARY KEY (carbonhydrates, lipids, proteins),
+lipids FLOAT NOT NULL CHECK (lipids >= 0),
+proteins FLOAT NOT NULL CHECK (proteins >= 0),
+PRIMARY KEY (carbonhydrates, lipids, proteins),
     CONSTRAINT fk_cooking_ingredients1
     FOREIGN KEY (ingredients_id)
     references cooking_ingredients(ingredients_id)
@@ -52,7 +50,7 @@ CREATE TABLE nutritions_info (
 
 DROP TABLE IF EXISTS type_meal;
 CREATE TABLE type_meal (
-	type_meal_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+type_meal_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     type_meal_name VARCHAR(100) NOT NULL,
     image varchar(100) not null check (image like 'https://%'),
     image_caption varchar(100) not null,
@@ -88,7 +86,8 @@ CREATE TABLE recipes (
     image_caption varchar(40) not null,
     PRIMARY KEY (recipe_id,ethnic_id,ingredients_id),
     CONSTRAINT fk_ethnic FOREIGN KEY (ethnic_id) references ethnic(ethnic_id)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
+    ON UPDATE NO ACTION ON DELETE RESTRICT,
+    -- to allaksa htan cascade
     CONSTRAINT fk_cooking_ingredients FOREIGN KEY (ingredients_id) references cooking_ingredients(ingredients_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
@@ -102,36 +101,36 @@ CREATE TABLE recipes (
 
 DROP TABLE IF EXISTS recipes_ingredients;
 CREATE TABLE recipes_ingredients (
-	ingredients_id INT UNSIGNED NOT NULL,
-	recipe_id INT UNSIGNED NOT NULL,
-	quantity FLOAT NOT NULL CHECK (quantity >= 0),
+ingredients_id INT UNSIGNED NOT NULL,
+recipe_id INT UNSIGNED NOT NULL,
+quantity FLOAT NOT NULL CHECK (quantity >= 0),
     primary key(ingredients_id,recipe_id),
     CONSTRAINT fk_recipes FOREIGN KEY (recipe_id) references recipes(recipe_id) ON UPDATE NO ACTION ON DELETE RESTRICT,
     CONSTRAINT fk_cooking_ingredients_recipes FOREIGN KEY (ingredients_id) references cooking_ingredients(ingredients_id) ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 
 #CREATE INDEX fk_recipes ON  recipes_ingredients(recipe_id);
-#CREATE INDEX fk_cooking_ingredients ON  recipes_ingredients(ingredients_id);
+CREATE INDEX fk_cooking_ingredients ON  recipes_ingredients(ingredients_id);
 
 
 DROP TABLE IF EXISTS recipes_type_meal;
 CREATE TABLE recipes_type_meal (
-	recipe_id INT UNSIGNED NOT NULL,
+recipe_id INT UNSIGNED NOT NULL,
     type_meal_id INT UNSIGNED NOT NULL,
     primary key(recipe_id,type_meal_id),
     CONSTRAINT fk_recipes_type_meal FOREIGN KEY (recipe_id) references recipes(recipe_id)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
+    ON UPDATE NO ACTION ON DELETE RESTRICT,
     CONSTRAINT fk_recipes_type_meal2 FOREIGN KEY (type_meal_id) references type_meal(type_meal_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
 #CREATE INDEX fk_recipes_type_meal ON  recipes_type_meal(type_meal_id);
-#CREATE INDEX fk_recipes_type_meal2 ON  recipes_type_meal(recipe_id);
+CREATE INDEX fk_recipes_type_meal1 ON  recipes_type_meal(recipe_id);
 
 
 
 DROP TABLE IF EXISTS tips;
 CREATE TABLE tips (
-	type_meal_id INT UNSIGNED NOT NULL,
+type_meal_id INT UNSIGNED NOT NULL,
     tip VARCHAR(200) NOT NULL,
     PRIMARY KEY (tip,type_meal_id),
     CONSTRAINT fk_tips FOREIGN KEY (type_meal_id) references type_meal(type_meal_id)
@@ -143,7 +142,7 @@ CREATE TABLE tips (
 
 DROP TABLE IF EXISTS meal;
 CREATE TABLE meal (
-	meal_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+meal_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     category_meal VARCHAR(100),
     image varchar(100) not null check (image like 'https://%'),
     image_caption varchar(100) not null,
@@ -153,29 +152,29 @@ CREATE TABLE meal (
 
 DROP TABLE IF EXISTS recipe_meals;
 CREATE TABLE recipe_meals (
-	meal_id INT UNSIGNED NOT NULL,
+meal_id INT UNSIGNED NOT NULL,
     recipe_id  INT UNSIGNED NOT NULL,
     primary key(meal_id,recipe_id),
     CONSTRAINT fk_recipe_meals FOREIGN KEY (meal_id) references meal(meal_id)
     ON UPDATE CASCADE ON DELETE RESTRICT,
     CONSTRAINT fk_recipe_meals2 FOREIGN KEY (recipe_id) references recipes(recipe_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
-); 
-#CREATE INDEX fk_recipe_meal ON  recipe_meals(meal_id);
+);
+CREATE INDEX fk_recipe_meal ON  recipe_meals(meal_id);
 #CREATE INDEX fk_recipe_meals2 ON  recipe_meals(recipe_id);
 
 DROP TABLE IF EXISTS steps;
 CREATE TABLE steps (
-	steps_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+steps_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
     steps_str VARCHAR(200) NOT NULL,
     image varchar(100) not null check (image like 'https://%'),
     image_caption varchar(100) not null,
     PRIMARY KEY (steps_id)
 );
-    
+   
 DROP TABLE IF EXISTS steps_recipes;
 CREATE TABLE steps_recipes (
-	steps_id INT UNSIGNED NOT NULL,
+steps_id INT UNSIGNED NOT NULL,
     recipe_id INT UNSIGNED NOT NULL,
     step_counter INT UNSIGNED NOT NULL,
     primary key(steps_id,recipe_id),
@@ -184,7 +183,7 @@ CREATE TABLE steps_recipes (
     CONSTRAINT fk_steps_recipes2 FOREIGN KEY (recipe_id) references recipes(recipe_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
     );
-#CREATE INDEX fk_steps_recipes ON  steps_recipes(steps_id);
+CREATE INDEX fk_steps_recipes ON  steps_recipes(steps_id);
 #CREATE INDEX fk_steps_recipes2 ON  steps_recipes(recipe_id);
 
 
@@ -211,7 +210,7 @@ CREATE TABLE recipes_equipment (
     CONSTRAINT fk_recipes_equipment2 FOREIGN KEY (equipment_id) references equipment(equipment_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-#CREATE INDEX fk_recipes_equipment ON  recipes_equipment(recipe_id);
+CREATE INDEX fk_recipes_equipment ON  recipes_equipment(recipe_id);
 #CREATE INDEX fk_recipes_equipment2 ON  recipes_equipment(equipment_id);
 
 
@@ -241,7 +240,7 @@ CREATE TABLE recipe_topics (
     topics_description VARCHAR(200) NOT NULL,
     image varchar(100) not null check (image like 'https://%'),
     image_caption varchar(100) not null,
-    PRIMARY KEY (topics_id) 
+    PRIMARY KEY (topics_id)
 );
 #CREATE unique INDEX  topics_pk ON recipe_topics(topics_id);
 
@@ -256,7 +255,7 @@ CREATE TABLE recipe_topics_recipes (
     CONSTRAINT fk_recipe_topics_recipes2 FOREIGN KEY (recipe_id) references recipes(recipe_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-#CREATE INDEX  fk_recipe_topics_recipes ON recipe_topics_recipes(topics_id);
+CREATE INDEX  fk_recipe_topics_recipes ON recipe_topics_recipes(topics_id);
 #CREATE INDEX  fk_recipe_topics_recipes2 ON recipe_topics_recipes(recipe_id);
  
 
@@ -287,7 +286,7 @@ CREATE TABLE cooker_ethnic (
     CONSTRAINT fk_cooker_ethnic2 FOREIGN KEY (ethnic_id) references ethnic(ethnic_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-#CREATE INDEX  fk_cooker_ethnic ON cooker_ethnic(cooker_id);
+CREATE INDEX  fk_cooker_ethnic ON cooker_ethnic(cooker_id);
 #CREATE INDEX  fk_cooker_ethnic2 ON cooker_ethnic(cooker_ethnic);
 
 DROP TABLE IF EXISTS cooker_recipes;
@@ -300,7 +299,7 @@ CREATE TABLE cooker_recipes (
     CONSTRAINT fk_cooker_recipe2 FOREIGN KEY (recipe_id) references recipes(recipe_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-#CREATE INDEX  fk_cooker_ethnic ON cooker_ethnic(cooker_id);
+CREATE INDEX  fk_cooker_ethnic ON cooker_ethnic(cooker_id);
 #CREATE INDEX  fk_cooker_ethnic2 ON cooker_ethnic(cooker_ethnic);
 
 DROP TABLE IF EXISTS episode;
@@ -322,18 +321,14 @@ CREATE TABLE episode_expansion (
     eval2 INT UNSIGNED CHECK (eval2 BETWEEN 1 AND 5),
     eval3 INT UNSIGNED CHECK (eval3 BETWEEN 1 AND 5),
     primary key(episode_id,season_year,cooker_id,recipe_id),
-    CONSTRAINT fk_episode_expansion FOREIGN KEY (episode_id, season_year) references episode(episode_id, season_year)
+    CONSTRAINT fk_episode_expansion1 FOREIGN KEY (episode_id, season_year) references episode(episode_id, season_year)
     ON UPDATE NO ACTION ON DELETE RESTRICT,
     CONSTRAINT fk_episode_expansion2 FOREIGN KEY (cooker_id) references cooker(cooker_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT,
     CONSTRAINT fk_episode_expansion3 FOREIGN KEY (recipe_id) references recipes(recipe_id)
     ON UPDATE NO ACTION ON DELETE RESTRICT
 );
-#CREATE INDEX  fk_episode_expansion ON episode_expansion(episode_id, season_year);
+-- idx_is_judge
+CREATE INDEX  fk_episode_expansion1 ON episode_expansion(episode_id, season_year);
 #CREATE INDEX  fk_episode_expansion2 ON episode_expansion(cooker_id);
 #CREATE INDEX  fk_episode_expansion3 ON episode_expansion(recipe_id);
-
-
-
-
-
